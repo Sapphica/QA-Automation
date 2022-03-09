c6d7d52bd8446bce984c6a71cfddb4e3b862e7a0
Sapphica <ms.sapphica@gmail.com>
2022-03-08 23:24:20 -0800

    new

---
+++ aos_tests.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python3  Line 1
+# -*- coding: Windows-1252 -*- Line 2
+
+import unittest
+import aos_locators as locators
+import aos_methods as methods
+
+methods.setUp()
+breakpoint()
+methods.adduser()
+methods.logout()
+methods.login()
+methods.logout()
+methods.tearDown()
+print('''             /\.../\          
+            (  •.•  )           
+             ..=*=..            
+        **~~( \.||./ )  ©Have a kitty day! ''')
+
+
+# class AosAppPositiveTestCases(unittest.TestCase):
+#
+#     @staticmethod
+#     def test_create_new_user():
+#         methods.setUp()
+#         methods.adduser()
+#         methods.logout()
+#         methods.login()
+#         methods.logout()
+#         methods.tearDown()
+#
+#
+
+
+
+
