0c2f260988c8398aeba2a16815ee3f9356a0a23b
Shawna Flannigan <ms.sapphica@gmail.com>
2022-03-14 14:58:26 -0700

    Version 1.4

--- aos_methods.py
+++ aos_methods.py
@@ -11,22 +11,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 #Headless
-# from selenium.webdriver.chrome.options import Options
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument("window-size=1400,1500")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("start-maximized")
-# options.add_argument("enable-automation")
-# options.add_argument("--disable-infobars")
-# options.add_argument("--disable-dev-shm-usage")
-# driver = webdriver.Chrome(options=options)
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)
 
 # none headless
-s = Service(executable_path='chromedriver.exe')
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
+# s = Service(executable_path='chromedriver.exe')
+# driver = webdriver.Chrome(service=s)
+# driver.maximize_window()
 
 st = 0.25  # sleep time
 driver.implicitly_wait(20)
